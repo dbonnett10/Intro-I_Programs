5c1126dbc8fbdfffedf2b7261c50dc745ea03e87
Daniel Bonnett <bonnettd@aldenv127.allegheny.edu>
2014-09-11 14:54:30 -0400

    Practical02

---
+++ practicals/practical02/Interesting.java
@@ -0,0 +1,20 @@
+//****************************************
+// Dan Bonnett
+// Practical 2, 11-12 September 2014
+// Prints Plankton, well kind of
+// ***************************************
+import java.util.Date;
+public class Interesting
+{
+    public static void main(String[] args)
+    {
+      System.out.println("Dan Bonnett, CMPSC 111/n" + new Date() + "/n");
+      System.out.println("    \\      /");
+      System.out.println("     \\____/    ");
+      System.out.println("   \\ /----\\ /  ");
+      System.out.println("    \\| 00 |/");
+      System.out.println("     | __ |");
+      System.out.println("     \\____/");
+      System.out.println("      |  |");
+     }
+}    
