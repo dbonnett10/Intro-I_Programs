954777e5bfe6e3cf788373f69322c378a8aadcc6
Daniel Bonnett <bonnettd@aldenv142.allegheny.edu>
2014-11-19 21:10:53 -0500

    added lab10 to bit bucket

---
+++ lab10/MandelbrotColor.java
@@ -0,0 +1,45 @@
+//**********************************************
+// Honor Code: The work I am submitting is a result of my own thinking and efforts
+// Dan Bonnett
+// CMPSC 111 Fall 2014
+// Lab 10
+//
+// Purpose: To explore some of the advanced graphical features that Java provides.
+//**********************************************
+
+import java.awt.Color;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class MandelbrotColor {
+    public static void main(String[] args) throws Exception {
+        int width = 1920, height = 1080, max = 1000;        // changing the max value will modify the quality of the image, and the time taken to produce it.
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        int black = 0;
+        int[] colors = new int[max];
+        for (int i = 0; i<max; i++) {
+            colors[i] = Color.HSBtoRGB(i/1024f, 1, i/(i+8f));   // changing the float value will modify the colors used in the image, and the elapsed time.
+        }
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                double c_re = (col - width/2)*4.0/width;
+                double c_im = (row - height/2)*4.0/width;
+                double x = 0, y = 0;
+                double r2;
+                int iteration = 0;
+                while (x*x+y*y < 4 && iteration < max) {
+                    double x_new = x*x-y*y+c_re;
+                    y = 2*x*y+c_im;
+                    x = x_new;
+                    iteration++;
+                }
+                if (iteration < max) image.setRGB(col, row, colors[iteration]);
+                else image.setRGB(col, row, black);
+            }
+        }
+
+        ImageIO.write(image, "png", new File("mandelbrot-color-float-1024.png"));   // name of the png file
+    }
+}
